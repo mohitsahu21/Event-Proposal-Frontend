9226d8add0331fcbbdd465491b9385bdf0377f46
mohitsahu21 <125088141+mohitsahu21@users.noreply.github.com>
2023-04-28 00:36:03 +0530

    Update Userprop.js

--- src/components/User/UserProposals/Userprop.js
+++ src/components/User/UserProposals/Userprop.js
@@ -42,7 +42,7 @@ function Userprop() {
   // };
 
   const getProposaldata = () => {
-    fetch("https://event-proposal-backend-g0mb.onrender.com/proposals", {
+    fetch("https://event-proposal-backend-ehjs.onrender.com/proposals", {
       method: "GET",
       crossDoamin: true,
       headers: {
